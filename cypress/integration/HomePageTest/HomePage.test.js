9eaf87ae576af4ad10d69f09f1269859d7444695
Sebbbbe <sebastian.tiger901@gmail.com>
2021-07-03 23:54:01 +0200

    added end2end cypress start

---
+++ cypress/integration/HomePageTest/HomePage.test.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress" />
+
+describe('Click create Issue button' , () => {
+    
+    it('Click button' , () => {
+        cy.visit('https://localhost:44322/')
+        
+    })
+})
